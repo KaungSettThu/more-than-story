9d357faacd0d332c4e7af0a46f9c317e56e57b98
KaungSettThu <46857482+KaungSettThu@users.noreply.github.com>
2021-01-14 13:41:23 +0630

    creating misc folder for database

--- add-article.php
+++ add-article.php
@@ -2,5 +2,12 @@
 
 <!-- this will be the page to add news article -->
 
+<form action="/includes/dbcontoller.php">
+
+<input type="text" placeholder="Article Title">
+<input type="text-box" placeholder="Article's Text">
+
+
+</form>
 
 <?php include('includes/footer.php'); ?>
